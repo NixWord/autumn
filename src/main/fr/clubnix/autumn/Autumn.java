5655e159f21eeece2842396ed5dc827bd9bbfd24
NixWord <lewinp@esiee.fr>
2011-01-09 18:57:15 +0100

    Creating project and first class

---
+++ src/main/fr/clubnix/autumn/Autumn.java
@@ -0,0 +1,15 @@
+package fr.clubnix.autumn;
+
+public class Autumn {
+
+	public void run(String[] args) {
+		
+	}
+
+	public static void main(String[] args) {
+		Autumn autumn = new Autumn();
+		
+		autumn.run(args);
+	}
+
+}
